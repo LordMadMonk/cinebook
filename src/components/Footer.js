48198bf173aaf60b977b1d7a112e407454263164
AnantCode101 <anantaay150@gmail.com>
2022-05-15 21:04:48 +0530

    updated home screen layout

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,20 @@
-import React from "react";
-import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
-import './Footer.css'
+/** @format */
+
+import React from 'react';
+import { MDBCol, MDBContainer, MDBRow, MDBFooter } from 'mdbreact';
+import './Footer.css';
 
 const Footer = () => {
   return (
-      <div className="footer">
-    <MDBFooter color="blue" className="font-small pt-4 mt-4">
-      <MDBContainer fluid className="text-center text-md-left">
-        <MDBRow>
-          <MDBCol md="12">
-            <h5 className="title"> Greeting from Creator Team of Cinebook</h5>
-            <p>
-              We hope you enjoyed the ticket booking experience.
-            </p>
-          </MDBCol>
-          {/* <MDBCol md="6">
+    <div className='footer'>
+      <MDBFooter color='blue' className='font-small pt-4 mt-4'>
+        <MDBContainer fluid className='text-center text-md-left'>
+          <MDBRow>
+            <MDBCol md='12'>
+              <h5 className='title'> Greeting from Creator Team of Cinebook</h5>
+              <p>We hope you enjoyed the ticket booking experience.</p>
+            </MDBCol>
+            {/* <MDBCol md="6">
             <h5 className="title">Links</h5>
             <ul>
               <li className="list-unstyled">
@@ -31,16 +31,16 @@ const Footer = () => {
               </li>
             </ul>
           </MDBCol> */}
-        </MDBRow>
-      </MDBContainer>
-      <div className="footer-copyright text-center py-3">
-        <MDBContainer fluid>
-          &copy; {new Date().getFullYear()} Copyright: Team Seven IBM
+          </MDBRow>
         </MDBContainer>
-      </div>
-    </MDBFooter>
+        <div className='footer-copyright text-center py-3'>
+          <MDBContainer fluid>
+            &copy; {new Date().getFullYear()} Copyright: Team Cinebook
+          </MDBContainer>
+        </div>
+      </MDBFooter>
     </div>
   );
-}
+};
 
 export default Footer;
